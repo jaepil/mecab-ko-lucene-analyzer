91d6793288da80962f83c326bd07ea552a9decf6
Yong-woon Lee <bibreen@gmail.com>
2015-03-08 00:51:10 +0900

    AllTest에 추가 안됐던 테스트 추가

--- lucene-analyzer/src/test/java/org/bitbucket/eunjeon/mecab_ko_lucene_analyzer/AllTests.java
+++ lucene-analyzer/src/test/java/org/bitbucket/eunjeon/mecab_ko_lucene_analyzer/AllTests.java
@@ -8,6 +8,8 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({
   MeCabKoStandardTokenizerTest.class,
   PosIdManagerTest.class,
-  TokenGeneratorWithStandardPosAppenderTest.class})
+  TokenGeneratorWithStandardPosAppenderTest.class,
+  TokenGeneratorWithSimilarityMeasurePosAppenderTest.class,
+  TokenGeneratorWithKeywordSearchPosAppenderTest.class})
 public class AllTests {
 }

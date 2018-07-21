36426c9ec1b728c60c72310ac5490a5f74840444
Jaepil Jeong <jaepil@kaist.ac.kr>
2018-07-21 12:55:52 +0900

    Update make-package.sh to support ES 6.3.x

--- make-package.sh
+++ make-package.sh
@@ -17,17 +17,13 @@ rm -rf $dir
 # make ElasticSearch plugin
 dir=$elasticsearch_analysis-$version
 mkdir $dir
-mkdir $dir/elasticsearch
-cp lucene-analyzer/target/$lucene_analyzer-$version-SNAPSHOT.jar $dir/elasticsearch/$lucene_analyzer-$version.jar
-cp mecab-loader/target/$mecab_loader-$version-SNAPSHOT.jar $dir/elasticsearch/$mecab_loader-$version.jar
-cp elasticsearch-analysis-mecab-ko/target/$elasticsearch_analysis-$elasticsearch_plugin_version-SNAPSHOT.jar $dir/elasticsearch/$elasticsearch_analysis-$elasticsearch_plugin_version.jar
-cp elasticsearch-analysis-mecab-ko/plugin-descriptor.properties $dir/elasticsearch/plugin-descriptor.properties
-cp elasticsearch-analysis-mecab-ko/plugin-security.policy $dir/elasticsearch/plugin-security.policy
-pushd $dir/elasticsearch
+cp lucene-analyzer/target/$lucene_analyzer-$version-SNAPSHOT.jar $dir/$lucene_analyzer-$version.jar
+cp mecab-loader/target/$mecab_loader-$version-SNAPSHOT.jar $dir/$mecab_loader-$version.jar
+cp elasticsearch-analysis-mecab-ko/target/$elasticsearch_analysis-$elasticsearch_plugin_version-SNAPSHOT.jar $dir/$elasticsearch_analysis-$elasticsearch_plugin_version.jar
+cp elasticsearch-analysis-mecab-ko/plugin-descriptor.properties $dir/plugin-descriptor.properties
+cp elasticsearch-analysis-mecab-ko/plugin-security.policy $dir/plugin-security.policy
+pushd $dir
 wget https://bitbucket.org/eunjeon/mecab-java/downloads/mecab-java-0.996.jar
 popd
-pushd $dir
-zip -r $elasticsearch_analysis-$elasticsearch_plugin_version.zip elasticsearch
-mv $elasticsearch_analysis-$elasticsearch_plugin_version.zip ../.
-popd
+zip -r $elasticsearch_analysis-$elasticsearch_plugin_version.zip $dir
 rm -rf $dir
